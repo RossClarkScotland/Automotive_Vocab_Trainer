cab78e0b990842f4a1ebb1c4369edb6bf17cc389
RossClarkScotland <rossclarkscotland@gmail.com>
2021-03-20 19:27:23 +0000

    Add comments for jQuery initializers

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
   $(document).ready(function(){
+    //Edge navbar to the right
     $('.sidenav').sidenav({edge: "right"});
+    //Initialize collapsible vocabulary cards
     $('.collapsible').collapsible();
+    //Initialize topics dropdown menues for add term
     $('select').formSelect();
+    //Initialize modals for action confirmation buttons
     $('.modal').modal();
   });
